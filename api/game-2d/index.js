ca397ce593bd99c0f193d11320fd99ae0015e5c2
soO <444deph12@gmail.com>
2023-06-15 22:26:42 -0700

    remove api

--- api/game-2d/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const app = express();
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "../../../game-2d/www")));
-
-module.exports = serverless(app);
--- api/game-3d/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const app = express();
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "../../../game-3d/dist")));
-
-module.exports = serverless(app);
